4ac276e4587564405fb42bf033bffc309360d900
Swapnali <maneswapnalisantosh@gmail.com>
2023-06-28 22:14:26 +0530

    Initial commit

---
+++ todo/todoapp/urls.py
@@ -0,0 +1,16 @@
+
+from django.urls import path
+from todoapp import views
+
+urlpatterns = [
+    path('dashboard/',views.dashboard),
+    path('register',views.user_register),
+    path('login',views.user_login),
+    path('logout',views.user_logout),
+    path('delete/<rid>',views.delete),
+    path('edit/<rid>',views.edit),
+    path('catfilter/<cv>',views.catfilter),
+    path('statfilter/<st>',views.statfilter),
+    path('datefilter/',views.datefilter),
+    path('datesort/<dv>',views.datesort),
+]
